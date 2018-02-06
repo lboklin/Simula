689b1ae21504364c5b17d49b8e9164c9553c7aed
Ludvig Böklin <ludvig.boklin@protonmail.com>
2018-02-06 14:20:00 +0100

    Sort the ever-growing list of dependencies

--- shell.nix
+++ shell.nix
@@ -3,20 +3,21 @@
 pkgs.haskell.lib.buildStackProject {
   name = "SimulaHS";
   inherit ghc;
-  buildInputs = with pkgs; [ mesa
-                             gcc7
-                             xorg.pixman
-                             wayland-protocols
-                             wayland
-                             xorg.libX11
-                             dbus
-                             weston
-                             (callPackage simula-openvr/openvr.nix { })
-                             libxml2
-                             libxkbcommon
-                             vulkan-loader
-                             steam
-                          ];
+  buildInputs = with pkgs; [
+    (callPackage simula-openvr/openvr.nix { })
+    dbus
+    gcc7
+    libxkbcommon
+    libxml2
+    mesa
+    steam
+    vulkan-loader
+    wayland
+    wayland-protocols
+    weston
+    xorg.libX11
+    xorg.pixman
+  ];
 
   LANG = "en_US.UTF-8";
   TMPDIR = "/tmp";
